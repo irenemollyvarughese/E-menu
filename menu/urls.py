621bb38b8ab2fb3d6fed64ca00b2c5ef04eb5a48
anjana@2003 <anjanapj21@gmail.com>
2025-07-30 16:11:06 +0530

    payment

--- menu/urls.py
+++ menu/urls.py
@@ -8,4 +8,19 @@ urlpatterns = [
     path('<str:qr_code>/add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('<str:qr_code>/remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('<str:qr_code>/cart/', views.view_cart, name='view_cart'),
+
+
+
+    
+    #  new add 
+
+
+    path('menu/<str:qr_code>/payment/', views.payment_page, name='payment_page'),
+    path('menu/<str:qr_code>/confirm-order/', views.confirm_order, name='confirm_order'),
+    path('menu/<str:qr_code>/card-payment/<int:order_id>/', views.card_payment, name='card_payment'),
+    path('menu/order-success/<int:order_id>/', views.order_success, name='order_success'),
+    path('order-status/<str:qr_code>/<int:order_id>/', views.track_order_view, name='track_order')
+ # 👈 New
+
+     
 ]
